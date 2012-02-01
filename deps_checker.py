9b8bfeb844fe969a01274a3f4368ebaaa6958f13
Marcel Stimberg <marcelcoding@gmail.com>
2012-02-01 23:26:00 +0100

    no longer ship (now outdated) external modules

--- deps_checker.py
+++ deps_checker.py
@@ -2,19 +2,6 @@ import os, sys
 
 RUN_FROM_DIR = os.path.abspath(os.path.dirname(sys.argv[0])) + '/'
 
-
-def run_cmds(cmds):
-  print
-  for cmd in cmds:
-    try:
-      print '=== RUNNING:', cmd
-      os.system(cmd)
-    except:
-      print '=== FAILED ===\n'
-      sys.exit()
-  print '=== SUCCESS ===\n'
-
-
 try:
   import sqlite3
 except:
@@ -36,51 +23,22 @@ try:
   print '    then go to http://127.0.0.1:8000/admin/'
   print
 except:
-  #traceback.print_exc()
-  cmds = [
-    'tar -zxf ext/Django-1.0.2-final.tar.gz Django-1.0.2-final/django',
-    'mv Django-1.0.2-final/django .',
-    'rmdir Django-1.0.2-final',
-  ]
-  print 'could not import django [http://www.djangoproject.com/].  try to install ourselves? ',
-  if sys.stdin.readline().strip().lower()=='yes':
-    run_cmds(cmds)
-  else:
-    print 'try the following:'
-    print '\n'.join(cmds)
-    sys.exit()
-
-try:
-  import deseb
-except:
-  #traceback.print_exc()
-  print 'could not import deseb [http://code.google.com/p/deseb/].  try running (from "%s"):' % RUN_FROM_DIR
-  print '\tsvn checkout http://deseb.googlecode.com/svn/trunk/src/deseb'
+  print 'could not import django [http://www.djangoproject.com/]. Try running  ',
+  print '\tsudo apt-get install python-django'
   sys.exit()
   
 try:
   import cairo
 except:
   #traceback.print_exc()
-  print 'could not import pycairo [http://cairographics.org/pycairo/].  try running (from "%s"):' % RUN_FROM_DIR
+  print 'could not import pycairo [http://cairographics.org/pycairo/]. Try running:' % RUN_FROM_DIR
   print '\tsudo apt-get install python-cairo'
 
 try:
   import poppler
 except:
-  cmds = [
-    'sudo apt-get install build-essential libpoppler-dev libpoppler-glib-dev python-cairo-dev bzr gnome-common python-dev python-gnome2-dev python-gtk2-dev python-gobject-dev python-pyorbit-dev',
-    'tar -zxf ext/pypoppler-0.8.1.tar.gz',
-    'cd pypoppler-0.8.1 && sh configure',
-    'cd pypoppler-0.8.1 && make',
-    'cd pypoppler-0.8.1 && sudo make install',
-  ]
-  print 'could not import pypoppler.  try to install ourselves? ',
-  if sys.stdin.readline().strip().lower()=='yes':
-    run_cmds(cmds)
-  else:
-    print 'try the following:'
-    print '\n'.join(cmds)
-    sys.exit()
+  #traceback.print_exc()
+  print 'could not import pypoppler [http://poppler.freedesktop.org/]. Try running:' % RUN_FROM_DIR
+  print '\tsudo apt-get install python-poppler'
 
 
