7867b981b3033d3993ec4a358e64421a30004e41
Marcel Stimberg <marcelcoding@gmail.com>
2012-02-12 21:38:33 +0100

    download full paper information from pubmed

--- importer/pubmed.py
+++ importer/pubmed.py
@@ -3,10 +3,14 @@ import urllib
 import BeautifulSoup
 
 from logger import log_debug, log_info, log_error
+from BeautifulSoup import BeautifulStoneSoup
+
+from gPapers.models import *
 
 BASE_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
 ESEARCH_QUERY = 'esearch.fcgi?db=pubmed&term=%s&usehistory=y'
 ESUMMARY_QUERY = 'esummary.fcgi?db=pubmed&query_key=%s&WebEnv=%s'
+EFETCH_QUERY = 'efetch.fcgi?db=pubmed&id=%s&retmode=xml'
 
 
 class PubMedSearch(object):
@@ -90,6 +94,68 @@ class PubMedSearch(object):
 
         self.search_cache[search_text] = papers
         return papers
+
+    def import_paper(self, data, paper=None):
+        assert paper.pubmed_id
+        log_info('Trying to import pubmed citation with id %s' % paper.pubmed_id)
+        query = BASE_URL + EFETCH_QUERY % paper.pubmed_id
+        try:
+            response = urllib.urlopen(query)
+            if not (response.getcode() == 200 or response.getcode() == 302):
+                log_error('Pubmed replied with error code %d for query: %s' % \
+                                  (response.getcode(), query))
+                return paper
+            parsed_response = BeautifulStoneSoup(response)
+
+            # Journal
+            try:
+                journal = parsed_response.findAll('journal')[0]
+                name = journal.findAll('title')[0].text
+                log_debug('Journal name: %s' % name)
+                try:
+                    issue = journal.findAll('issue')[0].text
+                except:
+                    issue = ''
+                log_debug('Journal issue: %s' % issue)
+                #TODO: Volume etc
+                source, created = Source.objects.get_or_create(name=name,
+                                                               issue=issue)
+                if created:
+                    source.save()
+                paper.source = source
+                paper.source_pages = parsed_response.findAll('medlinepgn')[0].text
+                log_debug('Pages: %s' % paper.source_pages)
+            except Exception as ex:
+                pass
+
+            # Title and abstract
+            try:
+                paper.title = parsed_response.findAll('articletitle')[0].text
+                log_debug('Title: %s' % paper.title)
+                paper.abstract = parsed_response.findAll('abstracttext')[0].text
+                log_debug('Abstract: %s' % paper.abstract)
+            except Exception as ex:
+                pass
+
+            # Authors
+            try:
+                authors = parsed_response.findAll('author')
+                for author in authors:
+                    author_name = author.forename.text + ' ' + \
+                                            author.lastname.text
+                    log_debug('\tAuthor: %s' % author_name)
+                    author_obj, created = Author.objects.get_or_create(name=author_name)
+                    paper.authors.add(author_obj)
+                    if created:
+                        author_obj.save()
+            except Exception as ex:
+                traceback.print_stack()
+
+        except Exception as ex:
+            log_error('Downloading paper with id %s failed: %s' % \
+                              (paper.pubmed_id, ex))
+
+        return paper
 
     def __str__(self):
         return "PubMed"
