8bf2facdffbb67e1cddc8d1d7476cb5ea68504dc
Marcel Stimberg <marcelcoding@gmail.com>
2012-04-09 22:29:00 +0200

    add copyright/license info to files

--- gpapers/__init__.py
+++ gpapers/__init__.py
@@ -2,21 +2,23 @@
 # -*- coding: utf-8 -*-
 
 #    gPapers
-#    Copyright (C) 2007 Derek Anderson
+#    Copyright (C) 2007-2009 Derek Anderson
+#                  2012      Derek Anderson and Marcel Stimberg
 #
-#    This program is free software; you can redistribute it and/or modify
+# This file is part of gPapers.
+#
+#    gPapers is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
+#    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
+#    gPapers is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#    You should have received a copy of the GNU General Public License
+#    along with gPapers.  If not, see <http://www.gnu.org/licenses/>.
 
 import commands, math, os, sys, thread, threading, time, traceback
 import mimetypes
--- gpapers/logger.py
+++ gpapers/logger.py
@@ -1,6 +1,25 @@
 '''
-Logging information for Brian
+Logging information for gPapers
 '''
+
+#    gPapers
+#    Copyright (C) 2007-2009 Derek Anderson
+#                  2012      Derek Anderson and Marcel Stimberg
+#
+# This file is part of gPapers.
+#
+#    gPapers is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    gPapers is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with gPapers.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 import sys
@@ -16,19 +35,24 @@ logging.getLogger('gPapers').addHandler(console)
 def log_error(message):
     logging.getLogger('gPapers').error(message)
 
+
 def log_warn(message):
     logging.getLogger('gPapers').warn(message)
+
 
 def log_info(message):
     logging.getLogger('gPapers').info(message)
 
+
 def log_debug(message):
     logging.getLogger('gPapers').debug(message)
+
 
 def log_level_error():
     '''Shows log messages only of level ERROR or higher.
     '''
     logging.getLogger('gPapers').setLevel(logging.ERROR)
+
 
 def log_level_warn():
     '''Shows log messages only of level WARNING or higher (including ERROR
@@ -36,11 +60,13 @@ def log_level_warn():
     '''
     logging.getLogger('gPapers').setLevel(logging.WARN)
 
+
 def log_level_info():
     '''Shows log messages only of level INFO or higher (including WARNING and
     ERROR levels).
     '''
     logging.getLogger('gPapers').setLevel(logging.INFO)
+
 
 def log_level_debug():
     '''Shows log messages only of level DEBUG or higher (including INFO,
