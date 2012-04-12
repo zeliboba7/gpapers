12fe108a940c0200f14b01b3aa47a8e0bba71198
Marcel Stimberg <marcelcoding@gmail.com>
2012-04-12 23:56:04 +0200

    also find DOIs that do not have any "doi" prefix

--- gpapers/importer/pdf_file.py
+++ gpapers/importer/pdf_file.py
@@ -30,7 +30,7 @@ from gpapers.logger import log_debug, log_info, log_error
 
 # A DOI consists of a numeric prefix starting with "10." followed by "/" and
 # a more or less arbitrary suffix
-p_doi = re.compile('doi\s*:?\s*(10.[0-9]+/[^/].+)\s', re.IGNORECASE)
+p_doi = re.compile('\s(10.[0-9]+/[^/].+)\s', re.IGNORECASE)
 
 
 def get_paper_info_from_pdf(data):
