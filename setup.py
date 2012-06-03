9bcd66620a6da5c451aea53bc695781fb360ca78
Marcel Stimberg <marcelcoding@gmail.com>
2012-06-04 00:19:48 +0200

    mode data and icons dir, adapt paths and setup.py script

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#! /usr/bin/python
 
 #    gPapers
 #    Copyright (C) 2007-2009 Derek Anderson
@@ -19,15 +19,35 @@
 #    You should have received a copy of the GNU General Public License
 #    along with gPapers.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 from distutils.core import setup
+from distutils.command.build_scripts import build_scripts as build_scripts_class
+
+
+class gpapers_build_scripts_class(build_scripts_class):
+    # Adjust bin/gpapers.py --> gpapers
+    # (adapted from code in zim written by Jaap Karssenberg: zim-wiki.org )
+
+    def run(self):
+        build_scripts_class.run(self)
+        if os.name == 'posix' and not self.dry_run:
+            for script in self.scripts:
+                fname, ext = os.path.splitext(script)
+                if ext == '.py':
+                    file = os.path.join(self.build_dir, script)
+                    file_no_ext = os.path.join(self.build_dir, fname)
+                    print 'renaming %s to %s' % (file, file_no_ext)
+                    os.rename(file, file_no_ext)
 
 setup(name='gPapers',
+      cmdclass={'build_scripts': gpapers_build_scripts_class},
       version='0.5dev',
       description='The Gnome-based Scientific Paper Organizer',
       author='Derek Anderson',
       author_email='public@kered.org',
       url='http://gpapers.org/',
-      packages=['gpapers', 'gpapers.gPapers', 'gpapers.importer', 'deseb'],
+      packages=['gpapers', 'deseb'],
+      package_data = {'gpapers': ['data/*', 'icons/*']},
       scripts=['gpapers.py'],
-      #TODO: Icons, UI files, desktop file
+      #TODO: desktop file
      )
