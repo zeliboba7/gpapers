cd58d9d154975b98b69ccdac0ab4eeddd153f5b1
Marcel Stimberg <marcelcoding@gmail.com>
2012-08-08 23:33:15 +0200

    fix __authors__

--- gpapers_info.py
+++ gpapers_info.py
@@ -5,6 +5,6 @@ having to import big modules.
 
 __version__ = '0.5dev'
 __copyright__ = 'Copyright (c) 2008, 2009 Derek Anderson; 2012 Derek Anderson & Marcel Stimberg'
-__authors__ = ['Derek Anderson <public@kered.org>', 'Marcel Stimberg <marcelcoding@googlemail.com']
+__authors__ = ['Derek Anderson <public@kered.org>', 'Marcel Stimberg <marcelcoding@googlemail.com>']
 __url__ = 'http://gpapers.org/'
 __description__ = 'The Gnome-based Scientific Paper Organizer'
